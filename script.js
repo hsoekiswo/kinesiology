4d3a2cd99212cbec2678b2de56f0d0aaa8f8f537
Hafizhun Soekiswo <hafizhun.soekiswo@efishery.com>
2023-09-28 12:42:29 +0700

    Add floating menu bar

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const btnBack = document.querySelector('.back-btn');
 btnBack.addEventListener('click', () => {
     window.history.back();
 })
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector('.header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
